22b7cec581c52908c54e7b7d30962628f22027fd
Paa Kwasi Owusu <paakwasi.owusu2023@gmail.com>
2025-05-15 20:21:29 +0100

    Led bar controlled with potentiometer

---
+++ Led_bar_potentiometer/Led_bar_potentiometer.ino
@@ -0,0 +1,22 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=2;i<10;i++){
+    pinMode(i,OUTPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float a = analogRead(0);
+  a = map(a,0,1023,0,9);
+
+  for(int i=0;i<=a;i++){
+    digitalWrite(i,HIGH);
+  }
+    for(int j= 9;j>a;j--){
+      digitalWrite(j,LOW);
+  }
+  delay(50);
+}
