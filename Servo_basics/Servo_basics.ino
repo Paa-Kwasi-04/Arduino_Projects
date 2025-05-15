eedbd3c77060e2531c47968f52c90b3714b5ad24
Paa Kwasi Owusu <paakwasi.owusu2023@gmail.com>
2025-05-15 22:07:07 +0100

    Servo motor basics

---
+++ Servo_basics/Servo_basics.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;int i,j;
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);  // using the servo library disables PWM function of digital pins
+  myservo.write(0);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(i=0;i<=180;i+= 20){  // Increases angle of motor to 180 with increment of 20
+    myservo.write(i);
+    delay(1000);
+  }
+  for(j=180;j>0;j-= 20){
+    myservo.write(j);
+    delay(1000);
+  }
+
+}
