09009a78f91438cbe83cd114f00e0fe4322d9bce
Paa-Kwasi-04 <paakwasi.owusu2023@gmail.com>
2025-11-11 07:21:33 +0000

    trying out a pidlib instead

--- RC_plant_PI_controller/RC_plant_PI_controller.ino
+++ RC_plant_PI_controller/RC_plant_PI_controller.ino
@@ -1,69 +1,60 @@
+#include "ArduPID.h"
+
+ArduPID myController;
+
+
 // Incremental PI (Tustin-discretized) - Ts = 1 ms
 const int Capacitor_Volt_Pin = A0;
 const int Pot_Volt_Pin = A1;
 const int pwmPin = 11;
-const float Vref = 5.0;
-const int ADCmax = 1023;
-const unsigned long Ts_ms = 1;   // 1 ms sample time
+//const float Vref = 5.0;
+//const int ADCmax = 1023;
+//const unsigned long Ts_ms = 1;   // 1 ms sample time
 
-// Discrete incremental coefficients (from Tustin)
-const float a = 0.96464;
-const float b = 0.982;
+double input;
+double output;
+double setpoint;
 
-
-// Limits (u in volts)
-const float u_min = 0.0;
-const float u_max = 5.0;
-
-float e = 0.0, e_prev = 0.0;
-float u = 0.0, u_prev = 0.0;
-
-
-
-unsigned long lastMillis = 0;
+double p = 0.95598;
+double i = 17.33192;
+double d = 0.0;
 
 void setup() {
   pinMode(pwmPin, OUTPUT);
   analogWrite(pwmPin, 0);
   Serial.begin(115200);
-  lastMillis = millis();
-  Serial.println("time(ms),Vcap,u_volts,pwm");
+  myController.begin(&input, &output, &setpoint, p, i, d);
+  myController.setSampleTime(1);      // OPTIONAL - will ensure at least 10ms have past between successful compute() calls
+  myController.setOutputLimits(0, 255);
+  myController.setBias(255.0 / 2.0);
+  myController.setWindUpLimits(-10, 10); // Groth bounds for the integral term to prevent integral wind-up
+  
+  myController.start();
+               
 }
 
 void loop() {
-  // run at ~1 ms intervals
-  if (millis() - lastMillis < Ts_ms) return;
-  lastMillis = millis();
-
   // measure Capacitor voltage 
-  int raw = analogRead(Capacitor_Volt_Pin);
-  float Vcap = (raw / (float)ADCmax) * Vref;
-
-  //Get Setpoint Voltage
-  int Setpoint_raw = analogRead(Pot_Volt_Pin);
-  float Setpoint = (Setpoint_raw / (float)ADCmax) * Vref;
+  input = analogRead(Capacitor_Volt_Pin);
   
-  // compute error
-  e = Setpoint - Vcap;
-
-  //  PI Control Algorithm 
-  u = u_prev + a*(e - b*e_prev); 
-
-  // map to PWM and write
-  int pwm = (int)round((u / Vref) * 255.0);
-  pwm = constrain(pwm, 0, 255);
-  analogWrite(pwmPin, pwm);
-
+  //Get Setpoint Voltage
+  setpoint = analogRead(Pot_Volt_Pin);
+  myController.compute();
+  myController.debug(&Serial, "myController", PRINT_INPUT    | // Can include or comment out any of these terms to print
+                                              PRINT_OUTPUT   | // in the Serial plotter
+                                              PRINT_SETPOINT |
+                                              PRINT_BIAS     |
+                                              PRINT_P        |
+                                              PRINT_I        |
+                                              PRINT_D);
+  
+  analogWrite(pwmPin, output); // Replace with plant control signal
   int resolution = 4;
 
-  // telemetry
-  Serial.print("Setpoint"); Serial.print(":");Serial.print(Setpoint,resolution); Serial.print(" , ");
-  Serial.print("Error_Signal"); Serial.print(":");Serial.print(e,resolution); Serial.print(" , ");
-  Serial.print("Capacitor Volt"); Serial.print(":");Serial.print(Vcap,resolution); Serial.print(" , ");
-  Serial.print("PID Signal"); Serial.print(":");Serial.print(u,resolution); Serial.print(" , ");
-  Serial.print("PWM Signal"); Serial.print(":");Serial.println(pwm);
 
-  // shift states
-  e_prev = e;
-  u_prev = u;
+  // telemetry
+  Serial.print("Setpoint"); Serial.print(":");Serial.print(setpoint); Serial.print(" , ");
+  Serial.print("Capacitor Volt"); Serial.print(":");Serial.print(input); Serial.print(" , ");
+  Serial.print("PID PWM Signal"); Serial.print(":");Serial.println(output);
+
 }
